b6ef64b64b103f29236c12e209fda2ea21f80eee
qinyan60229-lgtm <qinyan60229@gmail.com>
2026-09-08 16:38:36 +0800

    09080440

---
+++ BradWeb/src/main/java/tw/brad/servlet/Brad01.java
@@ -0,0 +1,23 @@
+package tw.brad.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet("/Brad01")
+public class Brad01 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Brad Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
