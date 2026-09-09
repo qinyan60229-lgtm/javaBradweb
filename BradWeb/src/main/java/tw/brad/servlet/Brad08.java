52179db325379b08df555aa91020ebfa6f2a10a9
qinyan <qinyan60229@gmail.com>
2026-09-10 01:43:13 +0800

    09100143

--- BradWeb/src/main/java/tw/brad/servlet/Brad08.java
+++ BradWeb/src/main/java/tw/brad/servlet/Brad08.java
@@ -12,7 +12,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.Part;
 
 @WebServlet("/Brad08")
-@MultipartConfig(
+@MultipartConfig(    //這支 Servlet 要處理 multipart/form-data，也就是檔案上傳  指定的暫存位置
 		location = "C:\\Users\\User\\git\\javaBradweb\\BradWeb\\src\\main\\webapp\\upload"
 		)
 public class Brad08 extends HttpServlet {
@@ -23,7 +23,7 @@ public class Brad08 extends HttpServlet {
 		String urip = request.getRemoteAddr();
 		
 		Collection<Part> parts = request.getParts();
-		for (Part part :parts) {
+		for (Part part :parts) {    //每一個 Part 都處理一次。
 //			String name = part.getName();
 //			System.out.println(name);
 			
@@ -37,7 +37,7 @@ public class Brad08 extends HttpServlet {
 			String sfname = String.format("%s_%s",
 			        urip.replace(":", "_"),
 			        part.getSubmittedFileName());
-			if (type != null && part.getSize() > 0) {
+			if (type != null && part.getSize() > 0) {  //這個 Part 是不是真的檔案，而且裡面有內容。
 				part.write(sfname);
 			}			
 		}
